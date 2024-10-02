d96d27c1f607a52ffdafb9e5408e905bd631ce4b
ramyareye <the.wicked.killah@gmail.com>
2024-10-02 18:17:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 80;
 
 app.use(express.static('public')); // Serve static files from the 'public' directory
 
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
